1ee620b80f2d1699771bb78586dbfa872ddd4335
Jaroslav Kaňka <kanka@jkanka.cz>
2020-03-26 00:36:44 +0100

    Prototype of calling from API.
    Prototype of GET request and it's usage in a project.
    
    Co-Authored-By: ondrastech82 <ondrastech82@users.noreply.github.com>

---
+++ prototype_api_use.py
@@ -0,0 +1,17 @@
+import json
+import requests
+
+
+apiCall = requests.get("https://coronavirus-19-api.herokuapp.com/all")
+loadJson = json.loads(apiCall.text)
+
+cases = loadJson.get('cases')
+deaths = loadJson.get('deaths')
+recovered = loadJson.get('recovered')
+
+debug_message_json = "Clear JSON: {raw_json}"
+debug_message_visible = "There's {number_of_cases} cases right now, {number_of_deaths} deaths and {number_of_recoveries} people recovered from the COVID-19."
+
+print(debug_message_json.format(raw_json = str(loadJson)))
+print(debug_message_visible.format(number_of_cases = str(cases), number_of_deaths = deaths, number_of_recoveries = recovered))
+
