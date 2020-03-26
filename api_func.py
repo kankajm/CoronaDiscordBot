5cb1b4a8ca0952ce9e661a492c69f1cbe492b397
Jaroslav Kaňka <kanka@jkanka.cz>
2020-03-26 15:47:14 +0100

    New API function.
    Gets data from API about one specific country.

--- api_func.py
+++ api_func.py
@@ -15,3 +15,33 @@ class api():
         return [cases, deaths, recovered]
 
 
+
+     # Gets data from API about one specific country. Output is array.
+     # WARNING: It's important to change int to str when it's used.
+    def country_corona(country):
+        api_call = requests.get(" https://coronavirus-19-api.herokuapp.com/countries/{}".format(country))
+        load_json = json.loads(api_call.text)
+
+        country_name = load_json.get('country')
+        cases = load_json.get('cases')
+        today_cases = load_json.get('todayCases')
+        deaths = load_json.get('deaths')
+        today_deaths = load_json.get('todayDeaths')
+        recovered_people = load_json.get('recovered')
+        active_cases = load_json.get('active')
+        critical_cases = load_json.get('critical')
+        cases_per_one_million_citizens = load_json.get('casesPerOneMillion')
+        deaths_per_one_million_citizens = load_json.get('deathsPerOneMillion')
+
+        return [country_name, cases, today_cases, deaths, today_deaths, recovered_people, active_cases, critical_cases, cases_per_one_million_citizens, deaths_per_one_million_citizens]
+
+
+
+# Tests --> PASSED
+# print(api.overview_corona())
+# print()
+# print(api.country_corona("Slovakia"))
+# print(api.country_corona("Italy"))
+# print(api.country_corona("USA"))
+
+
