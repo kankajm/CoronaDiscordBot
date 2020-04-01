f9df7ba8b8fe68d6074ba3e7829e04eee55ac5d8
Jaroslav Kaňka <kanka@jkanka.cz>
2020-04-01 14:58:37 +0200

    Version 2.1.1
    New activity_name (.corona help),
    New commands:
    .corona invite
    .corona help
    .corona world (same as overview)
    
    Some commands we're fixed or changed:
    .corona S.Korea (Beacuse S. Korea wasn't compatible with discord.py)
    .corona Czechia (Added day of the first case)
    
    Co-Authored-By: ondrastech82 <ondrastech82@users.noreply.github.com>

--- app.py
+++ app.py
@@ -34,11 +34,13 @@ async def corona(ctx, country_userinput):
     # Gives info about the Czech Republic
     if country_userinput == "CZ" or country_userinput == "cz" or country_userinput == "czech" or country_userinput == "czechia" or country_userinput == "Czechia" or country_userinput == "Czech":
         data = mzcr.covid_scrap()
+        data_two = api_func.api.country_corona("Czechia")
+        first_case = data_two[10].rstrip(" ")
         await ctx.send(f'According to Ministry of Health of the Czech Republic, Czechia has tested {data[2]} people. To this date Czechia have {data[3]} cases,'
-                       f' {data[5]} deaths and {data[4]} people recovered from the COVID-19. Source: http://tiny.cc/mzcr-covid , {data[0]}.')
+                       f' {data[5]} deaths and {data[4]} people recovered from the COVID-19. First case was detected on {first_case}. Source: http://tiny.cc/mzcr-covid , {data[0]}.')
     else:
         # .corona overview prints overview about the situation
-        if country_userinput == "overview":
+        if country_userinput == "overview" or country_userinput == "world":
             data = api_func.api.overview_corona()
             await ctx.send(f"{ctx.message.author.mention}, there's {data[0]} cases in a world right now, {data[1]} deaths and {data[2]} people recovered from the COVID-19.")
         else:
@@ -47,24 +49,60 @@ async def corona(ctx, country_userinput):
                 info = misc_func.symptoms_info()
                 await ctx.send(f"{ctx.message.author.mention}, {info} Source: http://tiny.cc/WHOLINK , WHO")
             else:
-                # .corona ping prints out ping of connection to Dicord API
-                if country_userinput == "ping":
-                    await ctx.send(f'Bots ping is {round(client.latency * 1000)}ms')
+                # Shows all the possible commands for this bot
+                if country_userinput == "help":
+                    embed = discord.Embed(title="Commands for the CoronaBot:", colour=discord.Color.red())
+                    embed.set_author(name=client.user.name, icon_url=client.user.avatar_url)
+                    embed.set_footer(text="This bot is proudly written in the Czech Republic by Jaroslav Kaňka and Ondřej Štěch.", icon_url="https://jkanka.cz/ikonka.png")
+
+                    embed.add_field(name="To show total numbers from countries all around the world use:",
+                                    value=".corona overview")
+                    embed.add_field(name="To show official numbers for the Czech Republic use:",
+                                    value=".corona czechia")
+                    embed.add_field(name="To show info and numbers about specific country use:",
+                                    value=".corona <country>")
+                    embed.add_field(name="To show verified info about symptoms of the COVID-19:",
+                                    value=".corona info")
+                    embed.add_field(name="To show version of the bot use:",
+                                    value=".corona version")
+                    embed.add_field(name="To show ping of the bot use:",
+                                    value=".corona ping")
+                    embed.add_field(name="To invite this bot on your server use:",
+                                    value=".corona invite")
+
+                    await ctx.send(embed=embed)
                 else:
-                    # .corona version prints out version number
-                    if country_userinput == "version":
-                        await ctx.send('Bot version is: {}'.format(parser.get('INFO', 'bot_version')))
+                    # .corona ping prints out ping of connection to Dicord API
+                    if country_userinput == "ping":
+                        await ctx.send(f'Bots ping is {round(client.latency * 1000)}ms')
                     else:
-                        # .corona <COUNTRY> prints out info about a single country of choice
-                        data = api_func.api.country_corona(country_userinput)
-                        if data == "error":
-                            await ctx.send(
-                                "You have written wrong country name or database is unavaible. Try it again.")
+                        # Sends invite link to add CoronaBot on their server
+                        if country_userinput == "invite":
+                            invite_link = parser.get('INFO', 'invite_link')
+                            await ctx.send(f'{ctx.message.author.mention}, to invite this bot on your server use: {invite_link}')
                         else:
-                            await ctx.send(f"{ctx.message.author.mention}"
-                                           f", {data[0]} has {data[1]} cases and {data[3]} deaths. Today there are {data[2]} cases and {data[4]} deaths. {data[5]} people recovered."
-                                           f" They're still {data[6]} active cases and {data[7]} people are in critical condition."
-                                           f" The concentration of cases in {data[0]} is {data[8]} cases per one milion citizens. First case was recognized on {data[10]}.")
+                            # Special command for South Korea because of their name in the API ("S. Korea" can't be used in one parameter command)
+                            if country_userinput == "S.Korea":
+                                data = api_func.api.country_corona("S. Korea")
+                                await ctx.send(f"{ctx.message.author.mention}"
+                                               f", {data[0]} has {data[1]} cases and {data[3]} deaths. Today there are {data[2]} cases and {data[4]} deaths. {data[5]} people recovered."
+                                               f" They're still {data[6]} active cases and {data[7]} people are in critical condition."
+                                               f" The concentration of cases in {data[0]} is {data[8]} cases per one milion citizens. First case was recognized on {data[10]}.")
+                            else:
+                                # .corona version prints out version number
+                                if country_userinput == "version":
+                                    await ctx.send('Bot version is: {}'.format(parser.get('INFO', 'bot_version')))
+                                else:
+                                    # .corona <COUNTRY> prints out info about a single country of choice
+                                    data = api_func.api.country_corona(country_userinput)
+                                    if data == "error":
+                                        await ctx.send(
+                                            "You have written wrong country name or database is unavaible. Try it again.")
+                                    else:
+                                        await ctx.send(f"{ctx.message.author.mention}"
+                                                       f", {data[0]} has {data[1]} cases and {data[3]} deaths. Today there are {data[2]} cases and {data[4]} deaths. {data[5]} people recovered."
+                                                       f" They're still {data[6]} active cases and {data[7]} people are in critical condition."
+                                                       f" The concentration of cases in {data[0]} is {data[8]} cases per one milion citizens. First case was recognized on {data[10]}.")
 
 
 # Getting env variable form os
